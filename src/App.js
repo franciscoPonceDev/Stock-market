c207ceeadb98e8bc97819d016b1dd55a2877c28a
Francisco Ponce <francisco.ponce.dev@gmail.com>
2022-04-13 11:58:31 -0300

    add styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import Header from './components/Header';
+import Home from './components/Home';
+import Details from './components/Details';
+import store from './redux/configureStore';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Provider store={store}>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/details/:idCompany" element={<Details />} />
+      </Routes>
+    </BrowserRouter>
+  </Provider>
+);
 
 export default App;
